edb880352e3e8317d25cd68887b62407bcb2e683
Alex Lepsa <lepsalex@gmail.com>
2016-04-10 16:42:49 -0400

    Number Wizard UI Complete

---
+++ number-wizard-ui/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Instance variables
+    int max;
+    int min;
+    int guess;
+
+    // Win condition
+    public int maxGuessesAllowed = 10;
+
+    // UI
+    public Text guessText;
+
+    // Use this for initialization
+    void Start() {
+        StartGame();
+    }
+    
+    // Game start
+    void StartGame() {
+        // Init game min/max variables
+        max = 1000;
+        min = 1;
+        NextGuess();
+    }
+    
+    // Calculate next guess
+    void NextGuess() {
+        // Guess a random num between min and max +1
+        // max + 1 because we want to include max in range
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+
+        // Check for win
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0) {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
+    }
+}
