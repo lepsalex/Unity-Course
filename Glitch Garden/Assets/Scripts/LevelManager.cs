253b90232827a6835c595f54c872e0589809ce2e
Alex Lepsa <lepsalex@gmail.com>
2016-10-01 13:52:50 -0400

    Starting out on Glitch Garden

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    void Start () {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel (string name) {
+        // Load Level
+        SceneManager.LoadScene (name);
+    }
+
+    public void LoadNextLevel () {
+        // Load Level
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitRequest () {
+        Debug.Log("Quit request recieved");
+        Application.Quit ();
+    }
+}
