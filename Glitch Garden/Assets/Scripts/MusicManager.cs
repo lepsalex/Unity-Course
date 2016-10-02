5f293edbb27c95822d69b26cc392a10089059d18
Alex Lepsa <lepsalex@gmail.com>
2016-10-02 01:15:45 -0400

    Menu system and scene transitions in place

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    void Awake () {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded (int level) {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+
+        if (thisLevelMusic) {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
