cdaa0f382b1f5ca89a3c8c2f250ab830f622db3a
Alex Lepsa <lepsalex@gmail.com>
2016-10-10 13:46:31 -0800

    Clean up of various scripts and configs

--- Glitch Garden/Assets/Scripts/LevelManager.cs
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -7,8 +7,8 @@ public class LevelManager : MonoBehaviour {
     public float autoLoadNextLevelAfter;
 
     void Start () {
-        if (autoLoadNextLevelAfter == 0) {
-            Debug.Log("Level auto load disabled");
+        if (autoLoadNextLevelAfter <= 0) {
+            Debug.Log("Level auto load disabled, use a positive number in seconds");
         } else {
             Invoke("LoadNextLevel", autoLoadNextLevelAfter);
         }
