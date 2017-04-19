ca63bd30bec7376b7527ff326ff3d1c78486190c
Alex Lepsa <lepsalex@gmail.com>
2017-04-18 22:11:19 -0400

    Downloaded Ahoy-Matey from Course GIT

---
+++ Ahoy-Matey/Assets/MyNetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+	
+	public void MyStartHost () {
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host.");
+		StartHost ();
+	}
+
+	public override void OnStartHost() {
+		Debug.Log (Time.timeSinceLevelLoad + " Host started.");
+	}
+
+	public override void OnStartClient(NetworkClient myClient) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client start requested.");
+		InvokeRepeating ("PrintDots", 0f, 1f);
+	}
+
+	public override void OnClientConnect(NetworkConnection conn) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connect to IP: " + conn.address);
+		CancelInvoke ();
+	}
+
+	void PrintDots () {
+		Debug.Log (".");
+	}
+}
