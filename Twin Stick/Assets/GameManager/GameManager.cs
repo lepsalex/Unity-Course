68fb77d35510e56a0f045a51417f018e032c50f6
Alex Lepsa <lepsalex@gmail.com>
2017-04-18 20:25:27 -0400

    Rewind System achieved

---
+++ Twin Stick/Assets/GameManager/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording = true;
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")) {
+            recording = false;
+        } else {
+            recording = true;
+        }
+	}
+}
---
+++ Twin Stick/Assets/Rewind/RewindSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindSystem : MonoBehaviour {
+
+    private GameManager gameManager;
+    private Stack<MyKeyFrame> keyFrames = new Stack<MyKeyFrame>(200);
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameManager.recording) {
+            Record();
+        } else {
+            PlayBack();
+        }
+    }
+
+    private void PlayBack() {
+        // If we haven't used up our stack ...
+        if (keyFrames.Count >= 0) {
+            rigidBody.isKinematic = true;
+
+            // Pop the latest frame and move to that position (the rewind)
+            MyKeyFrame frame = keyFrames.Pop();
+            transform.position = frame.pos;
+            transform.rotation = frame.rot;
+        }
+    }
+
+    private void Record () {
+        rigidBody.isKinematic = false;
+
+        // Stack the next frame 
+        keyFrames.Push(new MyKeyFrame(Time.time, transform.position, transform.rotation));
+    }
+}
+
+/// <summary>
+/// A structure for storing time, rotation, and position.
+/// </summary>
+public struct MyKeyFrame {
+    public float time;
+    public Vector3 pos;
+    public Quaternion rot;
+
+    public MyKeyFrame (float aTime, Vector3 aPos, Quaternion aRot) {
+        time = aTime;
+        pos = aPos;
+        rot = aRot;
+    }
+}
